c59cff7b350cdf518b6edff126f4eed982f83d6f
Attila Zseller <attila.zseller@prezi.com>
2015-03-01 19:36:58 +0100

    enabled scheduling

--- src/main/java/org/zs/audit/ScheduledTasks.java
+++ src/main/java/org/zs/audit/ScheduledTasks.java
@@ -5,10 +5,12 @@ import java.util.Date;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.zs.audit.model.Graph;
 
+@Configuration
 @EnableScheduling
 public class ScheduledTasks {
 	final static Logger LOGGER = Logger.getLogger(ScheduledTasks.class);
@@ -16,11 +18,11 @@ public class ScheduledTasks {
 	@Autowired
 	Graph graph;
 	
-	@Scheduled(cron="*/5 * * * * *")
+	@Scheduled(cron="0 */5 * * * *")
 	public void cleanupGraph() {
 		Calendar untilCalendar = Calendar.getInstance();
 		untilCalendar.setTime(new Date());
-		untilCalendar.add(Calendar.MINUTE, -1);
+		untilCalendar.add(Calendar.HOUR, -2);
 		Date until = untilCalendar.getTime();
 		synchronized (graph) {
 			LOGGER.info("Cleaning up logs older than " + until);
